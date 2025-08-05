d7c7131801c25d3eefdd2a025a69b6a0779b6cec
MatiasOrcajo <matiasorcajo3@gmail.com>
2025-08-04 21:52:13 -0300

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="icon" href="{{ asset('PERFIL-05.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('PERFIL-05.png') }}" type="image/x-icon">
+
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <link type="text/css" rel="stylesheet" href="{{asset('css/styles.css')}}">
